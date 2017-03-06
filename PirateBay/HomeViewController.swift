30f7fc81cebb2b482019f53f2cbc086ce321c5a5
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-03-06 15:22:21 -0800

    Added functionality to allow for container view to show page images and then scroll through them continuously.

--- PirateBay/HomeViewController.swift
+++ PirateBay/HomeViewController.swift
@@ -11,16 +11,75 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var pageView: UIView!
+    
+    var pageViewController : UIPageViewController?
+    var arrPageImage = ["piratebattle.jpg", "piratemap.jpg", "piratesofcarribeanposter.png"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setPageViewController()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setPageViewController() {
+        let pageVC = self.storyboard?.instantiateViewController(withIdentifier: "promoPageVC") as! UIPageViewController
+        pageVC.dataSource = self
+        
+        let firstController = getViewController(atIndex: 0)
+        
+        pageVC.setViewControllers([firstController], direction: .forward, animated: true, completion: nil)
+        
+        self.pageViewController = pageVC
+        self.addChildViewController(self.pageViewController!)
+        self.pageView.addSubview(self.pageViewController!.view)
+        self.pageViewController?.didMove(toParentViewController: self)
+        
     }
+    
+    fileprivate func getViewController(atIndex index: Int) -> PromoContentViewController {
+        
+        let promoContentVC = self.storyboard?.instantiateViewController(withIdentifier: "promoContentVC") as! PromoContentViewController
+        
+        promoContentVC.imageName = arrPageImage[index]
+        promoContentVC.pageIndex = index
+        
+        return promoContentVC
+        
+    }
+}
 
-
+extension HomeViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let pageContentVC = viewController as! PromoContentViewController
+        var index = pageContentVC.pageIndex
+        
+        if index == 0 || index == NSNotFound {
+            return getViewController(atIndex: arrPageImage.count - 1)
+        }
+        
+        index -= 1
+        
+        return getViewController(atIndex: index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        let pageContentVC = viewController as! PromoContentViewController
+        var index = pageContentVC.pageIndex
+        
+        if index == 0 || index == NSNotFound {
+            return nil
+        }
+        
+        index += 1
+        
+        if index == arrPageImage.count {
+            return getViewController(atIndex: 0)
+        }
+        
+        return getViewController(atIndex: index)
+    }
 }
 
--- PromoContentViewController.swift
+++ PromoContentViewController.swift
@@ -11,26 +11,16 @@ import UIKit
 class PromoContentViewController: UIViewController {
 
     @IBOutlet weak var promoImageView: UIImageView!
+    
+    var pageIndex = 0
+    var imageName : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let currentImage = imageName {
+            promoImageView.image = UIImage(named: currentImage)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
